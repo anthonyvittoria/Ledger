5ac58cb061ecf655bf2736676d2323dfa798e7ef
Anthony Vittoria <a.vittoria0304@gmail.com>
2019-07-18 15:06:17 -0700

    Add Django Suit

--- apps.py
+++ apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from suit.apps import DjangoSuitConfig
 
+class SuitConfig(DjangoSuitConfig):
+    layout = 'horizontal'
 
 class LedgerConfig(AppConfig):
     name = 'Ledger'
