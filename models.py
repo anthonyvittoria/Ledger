b204665dba27a120182f739f0e00b3aa5ac46bdb
Anthony Vittoria <a.vittoria0304@gmail.com>
2019-07-22 13:01:22 -0700

    Change sale model to reflect budget model

--- models.py
+++ models.py
@@ -83,11 +83,33 @@ class Budget(models.Model):
         return str(self.location) + ", " + str(self.customer) + " " + str(self.year)
 
 class Sale(models.Model):
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.ForeignKey(Location, on_delete=models.PROTECT)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     capability = models.ForeignKey(Capability, on_delete=models.PROTECT)
-    date = models.DateField(auto_now=False, auto_now_add=False)
+    jan = models.IntegerField(default=None)
+    feb = models.IntegerField(default=None)
+    mar = models.IntegerField(default=None)
+    apr = models.IntegerField(default=None)
+    may = models.IntegerField(default=None)
+    jun = models.IntegerField(default=None)
+    jul = models.IntegerField(default=None)
+    aug = models.IntegerField(default=None)
+    sep = models.IntegerField(default=None)
+    oct = models.IntegerField(default=None)
+    nov = models.IntegerField(default=None)
+    dec = models.IntegerField(default=None)
+    year = models.IntegerField(default=None)
+    q1 = models.IntegerField(default=None)
+    q2 = models.IntegerField(default=None)
+    q3 = models.IntegerField(default=None)
+    q4 = models.IntegerField(default=None)
+
+    def save(self, *args, **kwargs):
+        self.q1 = (self.jan + self.feb + self.mar)
+        self.q2 = (self.apr + self.may + self.jun)
+        self.q3 = (self.jul + self.aug + self.sep)
+        self.q4 = (self.oct + self.nov + self.dec)
+        super(Sale, self).save(*args, **kwargs)
 
     def __str__(self):
-        return str(self.customer) + ", " + str(self.location) + ", " + str(self.date)
+        return str(self.location) + ", " + str(self.customer) + " " + str(self.year)
