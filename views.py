b07417ef41beb05a3e732b8ccf585cdaa0536e7e
Anthony Vittoria <a.vittoria0304@gmail.com>
2019-07-29 11:25:20 -0700

    Remove unused imports

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.forms import inlineformset_factory
 
