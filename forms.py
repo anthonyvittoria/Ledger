1c63b9b3c4c39384fd22ab3a1f523eae660dbf2f
Anthony Vittoria <a.vittoria0304@gmail.com>
2019-08-08 11:06:36 -0700

    Update budget inline formset

--- forms.py
+++ forms.py
@@ -13,22 +13,15 @@ class BudgetForm(forms.ModelForm):
             'year',
         ]
 
-BudgetFormSet = forms.modelformset_factory(
-    models.Budget,
-    form=BudgetForm,
-)
-
-BudgetInlineFormSet = forms.inlineformset_factory(
+BudgetFormSet = forms.inlineformset_factory(
     models.Location,
     models.Budget,
     extra=1,
     fields=(
-        'customer',
+        'year', 'customer',
         'jan', 'feb', 'mar', 'apr',
         'may', 'jun', 'jul', 'aug',
         'sep', 'oct', 'nov', 'dec',
-        'year',
     ),
-    formset=BudgetFormSet,
     min_num=4,
 )
